afe2d9391d3151206bef08ebc59356f45cd84bd5
omegatech-andre <omega.ti_ind@outlook.com>
2023-10-24 17:12:35 -0300

    arquitetura basica do projeto

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import '../styles/globals.scss'
+
+
+export const metadata = {
+  title: 'Ômega Screen | Tintas Serigráficas',
+  description: 'Indústria especializada em tintas serigráficas, produzindo um padrão de qualidade desde 2021 é referência em Pernambuco.'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
