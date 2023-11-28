d148bd1a249c555bad43c66849a7f55da99f6a63
omegatech-andre <omega.ti_ind@outlook.com>
2023-11-28 10:40:29 -0300

    adicionando pagina /produto

---
+++ app/produto/page.jsx
@@ -0,0 +1,18 @@
+import BuscaCategoria from '@/components/_ui/buscaCategoria/BuscaCategoria'
+import './index.scss'
+
+export default function PageProduto() {
+  return (
+    <>
+      <div className="pageproduto">
+        <div className="pageproduto__indicador"></div>
+        <div className="pageproduto__content">
+          <h1>Com mais cor e intensidade para sua estampa!</h1>
+          <h2>Nossas linhas de produtos contam com a qualidade do Padrão Ômega!</h2>
+          <p>Navegue por categoria e encontre o produto ideal</p>
+        </div>
+        <BuscaCategoria/>
+      </div>
+    </>
+  )
+}
