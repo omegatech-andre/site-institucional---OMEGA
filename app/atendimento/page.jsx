bb1c1774d21885719b69b24abacf5c89637f66c9
omegatech-andre <omega.ti_ind@outlook.com>
2023-11-29 16:58:56 -0300

    adicionando pagina /atendimento

---
+++ app/atendimento/page.jsx
@@ -0,0 +1,27 @@
+import ButtonAtv from '@/components/_ui/buttonAtv/ButtonAtv'
+import './index.scss'
+
+export default function PageAtendimento() {
+  return (
+    <>
+      <div className="pageatendimento">
+        <div className="pageatendimento__indicador"></div>
+        <div className="pageatendimento__content">
+          <h1>Fale com o time mais intenso da serigrafia! </h1>
+          <h2>Fale com o nosso time comercial e faça um orçamento agora, ou tire suas dúvidas na área de perguntas frequentes</h2>
+        </div>
+        <div className="pageatendimento__box">
+          <div className="pageatendimento__boxbackground"></div>
+          <div className="pageatendimento__boxcontent">
+            <div className="boxcontent__content">
+              <img src="/assets/icons/wpp-icon.png" alt="" />
+              <h3>Fale agora com nosso time comercial, nosso atendimento está pronto para tirar todas as suas dúvidas</h3>
+              <ButtonAtv link="" largura="" nome="fale conosco"/>
+              <p>Ou ligue para o número de telefone do comercial (81) 98281-0058</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
