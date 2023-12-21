8de8bd8706a810d8469a1d3563058be2095ec5f9
omegatech-andre <omega.ti_ind@outlook.com>
2023-12-21 16:44:46 -0300

    version 1.0

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,8 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <body className='raiz'>
         {children}
-        <InBoundMkt/>
+        <InBoundMkt />
+        {'<!--Esse projeto está na v.1.0 e foi desenvolvido por André Campos (https://github.com/DDR23)-->'}
       </body>
     </html>
   )
