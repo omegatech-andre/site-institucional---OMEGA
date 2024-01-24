2797f0aadc82d1bf8e8949a4ad0ad664907d8c30
omegatech-andre <omega.ti_ind@outlook.com>
2024-01-24 10:56:17 -0300

    fix:{ajustes no layout geral}, feat:{foi adicionado o novo formato do inboundmkt}

--- components/_ui/estampadorDeSucesso/EstampadorDeSucesso.jsx
+++ components/_ui/estampadorDeSucesso/EstampadorDeSucesso.jsx
@@ -6,14 +6,16 @@ import CarrosselEstampador from "../estampadorCarrossel/CarrosselEstampador"
 export default function EstampadorDeSucesso() {
     return (
         <>
-            <section className="estampador">
-                <CarrosselEstampador />
-                <div className="estampador__cta">
-                    <Image src="/assets/images/logo-estampador-de-sucesso.png" width={200} height={130} alt=''></Image>
-                    <h1 className="description-hidden">A capacitação do estampador é um dos lados sociais da nossa indústria, formamos em nosso C.T (Centro de Treinamento) Estampadores de Sucesso, contribuindo no desenvolvimento do mercado serigráfico!</h1>
-                    <ButtonAtv link="" largura="" nome="quero participar"/>
-                </div>
-            </section>
+            <div className="carroestamp">
+                <section className="estampador">
+                    <CarrosselEstampador />
+                    <div className="estampador__cta">
+                        <Image src="/assets/images/logo-estampador-de-sucesso.png" width={200} height={130} alt=''></Image>
+                        <h1 className="description-hidden">A capacitação do estampador é um dos lados sociais da nossa indústria, formamos em nosso C.T (Centro de Treinamento) Estampadores de Sucesso, contribuindo no desenvolvimento do mercado serigráfico!</h1>
+                        <ButtonAtv link="" largura="" nome="quero participar" />
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
--- components/_ui/inBoundMkt/InBoundMkt.jsx
+++ components/_ui/inBoundMkt/InBoundMkt.jsx
@@ -1,37 +1,125 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
 import './index.scss'
+import usePost from '@/components/hooks/usePost'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { schemaManual } from '@/components/schema/schemaManual'
+import Link from 'next/link'
 
 export default function InBoundMkt() {
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schemaManual)
+  })
   const [isVisible, setIsVisible] = useState(false)
+  const [posted, setPosted] = useState(false)
+  const [data, setData] = useState('')
+
+  console.log('visivel?', isVisible)
+  console.log('posted', posted)
+
+  const { isPosted, isPosting, error } = usePost('https://sheetdb.io/api/v1/tfzirgx9wvfk1', data, posted)
+  
+  const submitForm = (data) => {
+    setPosted(true)
+    setData(data)
+  }
 
   useEffect(() => {
-    const show = () => {
-      setIsVisible(true)
-      setTimeout(hide, 4000)
+    if(isPosted || error){
+      // setPosted(false)
     }
+  }, [isPosted, error])
+  
 
-    const hide = () => {
-      setIsVisible(false)
-      setTimeout(show, 22000)
-    }
+  if (error) {
+    return (
+      <>
+        <div className="inboundmkt">
+          <img className={isVisible ? 'btnvisible' : 'btnhidden'} onClick={() => setIsVisible(!isVisible)} src={isVisible ? '/assets/icons/inboundmkton.png' : '/assets/icons/inboundmktoff.png'} alt="" />
+          {isVisible && (
+            <div className='inboundmktmodal'>
+              <div className="inboundmktmodal__content">
+                <p>Não foi possível completar o download</p>
+                <input className="btnenviar" type='button' value="tentar novamente" onClick={submitForm} />
+              </div>
+            </div>
+          )}
+        </div>
+      </>
+    )
+  }
 
-    setTimeout(show, 22000)
-  }, [])
+  if(isPosting){
+    return(
+      <>
+        <div className="inboundmkt">
+          <img className={isVisible ? 'btnvisible' : 'btnhidden'} onClick={() => setIsVisible(!isVisible)} src={isVisible ? '/assets/icons/inboundmkton.png' : '/assets/icons/inboundmktoff.png'} alt="" />
+          {isVisible && (
+            <div className='inboundmktmodal'>
+              <div className="inboundmktmodal__content">
+                <p>Só um momento...</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </>
+    )
+  }
+
+  if(isPosted){
+    return(
+      <>
+        <div className="inboundmkt">
+          <img className={isVisible ? 'btnvisible' : 'btnhidden'} onClick={() => setIsVisible(!isVisible)} src={isVisible ? '/assets/icons/inboundmkton.png' : '/assets/icons/inboundmktoff.png'} alt="" />
+          {isVisible && (
+            <div className='inboundmktmodal'>
+              <div className="inboundmktmodal__content">
+                <p>Receba o Manual clicando no botão abaixo</p>
+                <Link href='https://raw.githubusercontent.com/omegatech-andre/site-institucional---OMEGA-data/main/public/dataProducts/line-baseagua/clear/clear-soft-ar/fispq---clear-soft-ar.pdf' target='_blank'>
+                  <input className="btnenviar" type="submit" value="baixar manual" onClick={() => setIsVisible(!isVisible)} />
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
       <div className="inboundmkt">
-        <div className='inboundmkt__content'>
-          <Link href="https://wa.me/5581982810058" target='_blank'>
-            <img src="/assets/icons/inboundmkt.png" alt="" />
-          </Link>
-        </div>
+        <img className={isVisible ? 'btnvisible' : 'btnhidden'} onClick={() => setIsVisible(!isVisible)} src={isVisible ? '/assets/icons/inboundmkton.png' : '/assets/icons/inboundmktoff.png'} alt="" />
         {isVisible && (
-          <div className='inboundmkt__modal'>
-            <span>Fale com a gente!!</span>
-            <div>.</div>
+          <div className='inboundmktmodal'>
+            <div className="inboundmktmodal__content">
+              <p>Conheça o Manual das Tintas Ômega</p>
+              <form onSubmit={handleSubmit(submitForm)}>
+                <input
+                  {...register('nome')}
+                  type="text"
+                  placeholder='Nome'
+                />
+                {errors.nome && (<span>{errors.nome.message}</span>)}
+                <input
+                  {...register('telefone')}
+                  type="text"
+                  placeholder='Telefone'
+                  minLength={11}
+                  maxLength={11}
+                />
+                {errors.telefone && (<span>{errors.telefone.message}</span>)}
+                <input
+                  {...register('email')}
+                  type="email"
+                  placeholder='E-mail'
+                />
+                {errors.email && (<span>{errors.email.message}</span>)}
+                <input className="btnenviar" type="submit" value="quero o manual" />
+              </form>
+            </div>
           </div>
         )}
       </div>
