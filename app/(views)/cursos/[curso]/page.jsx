b15c3eb207f2efa508f4c7ef2b4471c9bfb6a16e
omegatech-andre <omega.ti_ind@outlook.com>
2024-05-30 09:27:11 -0300

    feat:{adicionada a seção de location}

--- app/(views)/cursos/[curso]/page.jsx
+++ app/(views)/cursos/[curso]/page.jsx
@@ -5,6 +5,7 @@ import CardEstampador from "@/components/_ui/estampadorDeSucesso/cardEstampador/
 import Palestrante from "@/components/_ui/estampadorDeSucesso/palestrante/Palestrante"
 import Modulo from "@/components/_ui/estampadorDeSucesso/modulo/Modulo"
 import CarrosselEstampador from "@/components/_ui/estampadorCarrossel/CarrosselEstampador"
+import Location from "@/components/_ui/estampadorDeSucesso/location/Location"
 
 export default function PageCursos() {
   return (
@@ -38,6 +39,12 @@ export default function PageCursos() {
         <div className="pagecursos__carrossel">
           <h1>Confira as <span>turmas</span> do Estampador de Sucesso</h1>
           <CarrosselEstampador />
+        </div>
+
+        {/* TODO COMENTARIOS SECTION */}
+
+        <div className="pagecursos__location">
+          <Location />
         </div>
         
       </div>
