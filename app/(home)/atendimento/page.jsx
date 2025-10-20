c52cd65caec7fb4d5e4120133dfa54b392e89326
oxdrtech <roboreserva.andre@gmail.com>
2025-10-20 10:02:45 -0300

    chore: manutencao da ordem de serviço N-0003

--- app/(home)/atendimento/page.jsx
+++ app/(home)/atendimento/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import ButtonAtv from '@/components/_ui/buttonAtv/ButtonAtv'
 import './index.scss'
 import FaqArea from '@/components/_ui/faqArea/FaqArea'
@@ -12,12 +13,11 @@ export default function PageAtendimento() {
           <h2>Fale com o nosso time comercial, solicite um orçamento ou, tire dúvidas na sessão: perguntas frequentes.</h2>
         </div>
         <div className="pageatendimento__box">
-          <div className="pageatendimento__boxbackground"></div>
           <div className="pageatendimento__boxcontent">
             <div className="boxcontent__content">
               <img src="/assets/icons/wpp-icon.png" alt="" />
               <h3>Fale agora com nosso time comercial, nosso atendimento está disponível para esclarecer todas dúvidas.</h3>
-              <ButtonAtv link="https://wa.me/5581982810058" target="_blank" largura="" nome="fale conosco" disabled=""/>
+              <ButtonAtv link="https://wa.me/5581982810058" target="_blank" largura="" nome="fale conosco"/>
               <p>Ou ligue para o número de telefone do comercial (81) 98281-0058</p>
             </div>
           </div>
