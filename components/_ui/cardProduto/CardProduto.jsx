0f8c269dab6224a05c2cbe08bbd693288b880aa6
oxdrtech <roboreserva.andre@gmail.com>
2025-10-31 00:12:32 -0300

    chore: manutencao da ordem de serviço N-0004

--- components/_ui/cardProduto/CardProduto.jsx
+++ components/_ui/cardProduto/CardProduto.jsx
@@ -1,60 +1,81 @@
-import { useEffect, useState } from 'react'
-import ButtonAtv from '../buttonAtv/ButtonAtv'
-import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri"
-import './index.scss'
+import { useEffect, useState } from "react";
+import ButtonAtv from "../buttonAtv/ButtonAtv";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import "./index.scss";
 
-export default function CardProduto({ nome, description, cores, pesos, images, fds, boletim }) {
-  const [imagemPrincipal, setImagemPrincipal] = useState(images[0])
-  const [inicio, setInicio] = useState(0)
-  const [numImagens, setNumImagens] = useState(5)
+export default function CardProduto({
+  nome,
+  description,
+  cores,
+  pesos,
+  images,
+  fds,
+  boletim,
+}) {
+  const [imagemPrincipal, setImagemPrincipal] = useState(images[0]);
+  const [inicio, setInicio] = useState(0);
+  const [numImagens, setNumImagens] = useState(5);
 
   useEffect(() => {
-    setImagemPrincipal(images[0])
-  }, [images])
+    setImagemPrincipal(images[0]);
+  }, [images]);
 
   useEffect(() => {
     const atualizarNumImagens = () => {
-      setNumImagens(window.innerWidth <= 320 ? 3 : 5)
-    }
+      setNumImagens(window.innerWidth <= 320 ? 3 : 5);
+    };
 
-    window.addEventListener('resize', atualizarNumImagens)
-    atualizarNumImagens()
+    window.addEventListener("resize", atualizarNumImagens);
+    atualizarNumImagens();
 
-    return () => window.removeEventListener('resize', atualizarNumImagens)
-  }, [])
+    return () => window.removeEventListener("resize", atualizarNumImagens);
+  }, []);
 
   const proximaImagem = () => {
-    setInicio((inicio + 1) % (images.length - numImagens + 1))
-  }
+    setInicio((inicio + 1) % (images.length - numImagens + 1));
+  };
 
   const imagemAnterior = () => {
-    setInicio((inicio - 1 + (images.length - numImagens + 1)) % (images.length - numImagens + 1))
-  }
+    setInicio(
+      (inicio - 1 + (images.length - numImagens + 1)) %
+        (images.length - numImagens + 1)
+    );
+  };
 
-  const imagensVisiveis = images.slice(inicio, inicio + numImagens)
+  const imagensVisiveis = images.slice(inicio, inicio + numImagens);
 
   return (
     <>
       <div className="cardproduto">
         <div className="cardproduto__img">
           <img src={imagemPrincipal} alt="" />
-          {
-            images.length > 1 && (
-              <div className="cardproduto__img--mini">
-                <div className='setas'>
-                  {images.length > numImagens && <div className='bgsetas' onClick={imagemAnterior}><RiArrowLeftSLine size={30} /></div>}
-                  {images.length > numImagens && <div className='bgsetas' onClick={proximaImagem}><RiArrowRightSLine size={30} /></div>}
-                </div>
-                <div className='images'>
-                  {
-                    imagensVisiveis.map((image, index) => (
-                      <img className='imagescarrossel' key={index} src={image} alt="" onClick={() => setImagemPrincipal(image)} />
-                    ))
-                  }
-                </div>
+          {images.length > 1 && (
+            <div className="cardproduto__img--mini">
+              <div className="setas">
+                {images.length > numImagens && (
+                  <div className="bgsetas" onClick={imagemAnterior}>
+                    <RiArrowLeftSLine size={30} />
+                  </div>
+                )}
+                {images.length > numImagens && (
+                  <div className="bgsetas" onClick={proximaImagem}>
+                    <RiArrowRightSLine size={30} />
+                  </div>
+                )}
               </div>
-            )
-          }
+              <div className="images">
+                {imagensVisiveis.map((image, index) => (
+                  <img
+                    className="imagescarrossel"
+                    key={index}
+                    src={image}
+                    alt=""
+                    onClick={() => setImagemPrincipal(image)}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
         </div>
         <div className="cardproduto__detalhes">
           <h1>{nome}</h1>
@@ -65,14 +86,15 @@ export default function CardProduto({ nome, description, cores, pesos, images, f
             <>
               <p>cores disponíveis:</p>
               <div className="cardproduto__detalhes--cores">
-                {
-                  cores.map((objetoCor, index) => (
-                    <div key={index}>
-                      <div className="coresbox" style={{ backgroundColor: objetoCor.color }}></div>
-                      <span>{objetoCor.name}</span>
-                    </div>
-                  ))
-                }
+                {cores.map((objetoCor, index) => (
+                  <div key={index}>
+                    <div
+                      className="coresbox"
+                      style={{ backgroundColor: objetoCor.color }}
+                    ></div>
+                    <span>{objetoCor.name}</span>
+                  </div>
+                ))}
               </div>
             </>
           )}
@@ -80,24 +102,31 @@ export default function CardProduto({ nome, description, cores, pesos, images, f
             <>
               <p>pesos disponíveis:</p>
               <div className="cardproduto__detalhes--pesos">
-                {
-                  pesos.map((peso, index) => (
-                    <span key={index}>{peso}</span>
-                  ))
-                }
+                {pesos.map((peso, index) => (
+                  <span key={index}>{peso}</span>
+                ))}
               </div>
             </>
           )}
           <div className="cardproduto__detalhes--boletins">
-            <ButtonAtv link={fds} largura="" nome="fds" />
-            <ButtonAtv link={boletim} largura="" nome="boletim técnico" />
+            <ButtonAtv
+              link={fds}
+              target="_blank"
+              largura=""
+              nome="fds"
+            />
+            <ButtonAtv
+              link={boletim}
+              target="_blank"
+              largura=""
+              nome="boletim técnico"
+            />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
-
 
 //  componente de ser chamado da forma abaixo
 //
